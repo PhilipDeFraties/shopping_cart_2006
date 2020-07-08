74c3356ab7e8af8d9f4bc40f0454914800a808cd
PhilipDeFraties <philipjfry338@gmail.com>
2020-07-08 10:24:35 -0600

    Add details to instance variables.

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,10 +1,11 @@
 class ShoppingCart
-  attr_reader :name, :capacity, :products
+  attr_reader :name, :capacity, :products, :details
 
   def initialize(name, capacity)
     @capacity = capacity
     @name = name
     @products = []
+    @details = {name: @name, capacity: @capacity}
   end
 
   def add_product(product)
