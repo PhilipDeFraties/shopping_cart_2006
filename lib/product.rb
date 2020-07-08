f2204680deed4f2ce599fdb7ac636998359d0f2b
PhilipDeFraties <philipjfry338@gmail.com>
2020-07-08 09:15:11 -0600

    Add instance variables and reader.

--- lib/product.rb
+++ lib/product.rb
@@ -1,7 +1,13 @@
 class Product
+  attr_reader :category, :item, :price, :quantity
 
-  def initialize(type, item, price, quantity)
+  def initialize(category, item, price, quantity)
+    @category = category
+    @item = item
+    @price = price
+    @quantity = quantity
   end
 
-  
+
+
 end
